646b106f4e98a272ae13f4a77b1dde0f2faeb487
Anthony Fu <anthonyfu117@hotmail.com>
2020-01-19 14:23:54 +0000

    chore: init

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  extends: [
+    '@antfu/eslint-config-ts',
+  ],
+  plugins: [
+    'jest',
+  ],
+  rules: {
+  },
+}
