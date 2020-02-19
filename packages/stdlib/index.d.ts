604185fcd5c0680662235bbd8a1027adb703c642
Anthony Fu <anthonyfu117@hotmail.com>
2020-02-19 10:37:00 +0800

    feat: stdlib package

---
+++ packages/stdlib/index.d.ts
@@ -0,0 +1,7 @@
+declare const StandardLibrary: {
+  js: Record<string, string>
+  py: Record<string, string>
+  default: Record<string, string>
+}
+
+export default StandardLibrary
---
+++ packages/stdlib/scripts/build.ts
@@ -0,0 +1,36 @@
+import path from 'path'
+import fs from 'fs-extra'
+import glob from 'fast-glob'
+
+function preprocess(code: string) {
+  // TODO: preprocess the code
+  return code
+}
+
+async function build() {
+  const libDir = path.resolve(__dirname, '../lib')
+  const filenames = await glob('**/*.wy', {
+    cwd: libDir,
+    onlyFiles: true,
+  })
+
+  const libs: Record<string, Record<string, string>> = {
+    js: {},
+    py: {},
+    default: {},
+  }
+
+  for (const filename of filenames) {
+    const filenameWithoutExt = filename.slice(0, -3)
+    let target = 'default'
+    let name = filenameWithoutExt
+    if (filenameWithoutExt.includes('/'))
+      [target, name] = filenameWithoutExt.split('/')
+    const code = await fs.readFile(path.join(libDir, filename), 'utf-8')
+    libs[target][name] = preprocess(code)
+  }
+
+  await fs.writeFile(path.resolve(__dirname, '../index.js'), `module.exports=${JSON.stringify(libs)}`, 'utf-8')
+}
+
+build()
